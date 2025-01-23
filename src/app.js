a98295c099adb64203c746ed7fd5e632852b0b26
Shekhu04 <gshikhar04@gmail.com>
2025-01-24 00:56:52 +0530

    made user and feed api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const User = require('./models/user');
 
 app.use(express.json());
 
+//SignUp Api
 app.post("/signup", async (req,res) => {
     //Creating a new instance of User model
     const user = new User(req.body);
@@ -15,9 +16,35 @@ app.post("/signup", async (req,res) => {
     }
     catch(err) {
         res.status(400).send("Error while saving user" + err.message);
-    }
-    
+    }   
 });
+
+//Get user by email
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+    const user = await User.findOne({emailId : userEmail});
+    if(!user){
+        res.status(404).send("User not found");
+    } else {
+        res.send(user);
+    }
+    } catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//Feed Api = GET /feed - get all the users from the database
+app.get("/feed", async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+})
 
 connectDB()
   .then(() => {
