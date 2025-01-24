84d4faec3b01e056eaa9ba78862b2d95fc9d492a
Shekhu04 <gshikhar04@gmail.com>
2025-01-24 12:28:08 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -63,17 +63,34 @@ app.delete("/user", async (req,res) => {
 })
 
 //Update data of the users
-app.patch("/user", async (req,res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
     try{
+         const ALLOWED_UPDATES = [
+           "photoUrl",
+           "about",
+           "skills",
+           "age",
+         ];
+
+         const isUpdateAllowed = Object.keys(data).every((k) => 
+           ALLOWED_UPDATES.includes(k)
+         );
+         if (!isUpdateAllowed) {
+          throw new Error("Invalid updates");
+         }
+         if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+         }
+
         const user = await User.findByIdAndUpdate({_id : userId}, data, {
             runValidators: true,
         });
         res.send("User updated successfully");
     } catch(err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("Update failed : " + err.message);
     }
 });
 
