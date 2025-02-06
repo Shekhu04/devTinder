a14e01ecf5af1d28febc2db5654a5ff0c132192a
Shekhu04 <gshikhar04@gmail.com>
2025-02-06 12:11:11 +0530

    backend completed

--- src/routes/user.js
+++ src/routes/user.js
@@ -38,9 +38,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       ],
     })
       .populate("fromUserId", USER_SAFE_DATA)
-      .populate("toUserId", USER_SAFE_DATA);
-
-    console.log(connectionRequests);
+      .populate("toUserId", USER_SAFE_DATA); 
 
     const data = connectionRequests.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
